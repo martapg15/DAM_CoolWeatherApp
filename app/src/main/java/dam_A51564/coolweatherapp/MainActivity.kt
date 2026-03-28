b1af8b4f74d9bdd9d122f932b6f0c428a9180137
Marta Garcia <martapinto934@gmail.com>
2026-03-28 21:02:50 +0000

    imported drawables. added the themes and colors

--- app/src/main/java/dam_A51564/coolweatherapp/MainActivity.kt
+++ app/src/main/java/dam_A51564/coolweatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package dam_A51564.coolweatherapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +8,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var day: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    setTheme(R.style.Theme_Night)
+                }
+            }
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day_Land)
+                } else {
+                    setTheme(R.style.Theme_Night_Land)
+                }
+            }
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
